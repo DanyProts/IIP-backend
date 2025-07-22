8a33336680dbb9d285bc6fb06a5ec5e1ed8f2c61
Maxim605 <komardin.macsim@gmail.com>
2025-07-22 18:19:07 +0300

    feat(gmail): adding authorization via gmail

--- app/user_service/models.py
+++ app/user_service/models.py
@@ -6,7 +6,7 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable=False)
-    email = Column(String(100), nullable=False, unique=True, index=True)
+    email = Column(String(100), nullable=False, unique=True, index=True) 
     password_hash = Column(Text, nullable=False)
     role = Column(String(20), default="student")  # e.g., 'student' or 'admin'
     avatar_url = Column(Text, nullable=True)
